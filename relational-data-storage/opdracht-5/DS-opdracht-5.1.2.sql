c5070bdcad28c30f49493cd0c297446c20f768eb
PatrickLubbers <134800717+PatrickLubbers@users.noreply.github.com>
2024-02-19 15:57:08 +0100

    #6 5.1.2 af

--- relational-data-storage/opdracht-5/DS-opdracht-5.1.2.sql
+++ relational-data-storage/opdracht-5/DS-opdracht-5.1.2.sql
@@ -1,12 +1,19 @@
 -- Selecteer uit de hitcount-tabel per jaar het aantal records, de minimale, maximale, gemiddelde en totale hitcount.
 
 SELECT 
+    mhl_hitcount.year,
+    mhl_hitcount.month,
     COUNT(*),
     MIN(mhl_hitcount.hitcount),
     MAX(mhl_hitcount.hitcount),
     AVG(mhl_hitcount.hitcount),
     SUM(mhl_hitcount.hitcount)
 FROM 
-    mhl_hitcount;
-
+    mhl_hitcount
+GROUP BY
+    mhl_hitcount.year,
+    mhl_hitcount.month
+ORDER BY
+    mhl_hitcount.year,
+    mhl_hitcount.month;
 
